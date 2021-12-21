0499c29b6bdd73706916aeac336bf9189effed5e
Greggles182 <76397590+Greggles182@users.noreply.github.com>
2021-12-21 13:34:15 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,54 @@
-basic.forever(function () {
-	
+enum RadioMessage {
+    message1 = 49434,
+    LightOff = 64509,
+    LightOn = 49786,
+    HeaterOn = 29523,
+    HeaterOff = 54869,
+    WaterTrigger = 8454
+}
+radio.onReceivedMessage(RadioMessage.LightOff, function () {
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
+radio.onReceivedMessage(RadioMessage.HeaterOff, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+})
+radio.onReceivedMessage(RadioMessage.HeaterOn, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+radio.onReceivedMessage(RadioMessage.WaterTrigger, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+radio.onReceivedMessage(RadioMessage.LightOn, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+})
+radio.setGroup(1)
